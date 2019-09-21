c36554e3e5f9fab743720046ffb57a4f0ee013bf
Tim Rossiter <rosstimo@isu.edu>
2019-09-21 16:30:35 -0600

    create ServoTest.py

---
+++ code/Tests/ServoTest.py
@@ -0,0 +1,28 @@
+from adafruit_servokit import ServoKit
+
+class ServoTest():
+
+    '''
+    see:    https://learn.adafruit.com/adafruit-16-channel-servo-driver-with-raspberry-pi/using-the-adafruit-library
+            https://learn.adafruit.com/adafruit-16-channel-servo-driver-with-raspberry-pi/library-reference
+
+    kit = ServoKit(channels=16)
+    kit.servo[0].angle = 180
+    kit.servo[0].actuation_range = 160
+    set_pulse_width_range(min_pulse, max_pulse)
+    kit.servo[0].set_pulse_width_range(1000, 2000)
+    '''
+
+  def usage(self):
+        '''Usage Examples:'''
+
+
+if __name__ == '__main__' :
+
+    docStr = ClozeQ.__doc__.splitlines()
+    for line in docStr:
+        print(line)
+
+    docStr = ClozeQ.usage.__doc__.splitlines()
+    for line in docStr:
+        print(line)
